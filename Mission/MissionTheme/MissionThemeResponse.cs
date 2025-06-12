aabeb13b381b9d99938029cdf0b8675ac339ec28
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-12 14:59:26 +0530

    Add files via upload

---
+++ Mission/MissionTheme/MissionThemeResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Mission.Entities.Models.MissionTheme
+{
+    public class MissionThemeResponse
+    {
+        public int Id { get; set; }
+        public string ThemeName { get; set; }
+        public string Status { get; set; }
+
+    }
+}
