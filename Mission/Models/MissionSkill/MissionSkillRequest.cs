cbe2c0309e93541aecfef70fe41f4922b08ff132
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-12 14:56:56 +0530

    Add files via upload

---
+++ Mission/Models/MissionSkill/MissionSkillRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mission.Entities.Models;
+
+namespace Mission.Entities.Models.MissionSkill
+{
+    
+    public class UpdateMissionSkillRequestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+
+}
---
+++ Mission/Models/MissionSkill/MissionSkillResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mission.Entities.Models.MissionSkill
+{
+    public class MissionSkillResponse
+    {
+        public int Id { get; set; }
+        public string SkillName { get; set; }
+        public string Status { get; set; }
+    }
+}
