9f895f09017da9ed85a8fe0c304687f2c02bcb5a
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-12 14:53:24 +0530

    Add files via upload

---
+++ Mission/Models/AddUserRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mission.Entities.Models
+{
+    public class AddUserRequestModel
+    {
+        public AddUserRequestModel()
+        {
+        }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string EmailAddress { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string UserType { get; set; } = "user"; // default to "user", can be changed
+    }
+}
---
+++ Mission/Models/UpdateUserRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mission.Entities.Models
+{
+    public class UpdateUserRequestModel
+    {
+        public int Id { get; set; } // Required to identify which user to update
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+        // Optional: you can add more fields if needed
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
