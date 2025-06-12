d100dfea7f53da3560ff8fc1afeb2f1e6fb5c71d
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-12 15:34:41 +0530

    Add files via upload

---
+++ Mission/Mission.Services/Services/AdminUserService.cs
@@ -0,0 +1,27 @@
+﻿using Mission.Entities.Models;
+using Mission.Repositories.Interface;
+using Mission.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services
+{
+    public class AdminUserService(IAdminUserRepository _adminUserRepository) : IAdminUserService
+    {
+        public List<UserDetails> UserDetailsList()
+        {
+            return _adminUserRepository.UserDetailsList();
+        }
+        public string UpdateUser(UpdateUserRequestModel model)
+        {
+            return _adminUserRepository.UpdateUser(model);
+        }
+        public string AddUser(AddUserRequestModel model)
+        {
+            return _adminUserRepository.AddUser(model);
+        }
+    }
+}
