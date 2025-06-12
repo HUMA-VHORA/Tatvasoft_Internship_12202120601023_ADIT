30ef0710d7f9f9ac5a8a317d81c89b830f1dbe05
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-12 15:33:23 +0530

    Add files via upload

---
+++ Mission/Mission.Services/IServices/IAdminUserService.cs
@@ -0,0 +1,16 @@
+﻿using Mission.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.IServices
+{
+    public interface IAdminUserService
+    {
+        List<UserDetails> UserDetailsList();
+        string UpdateUser(UpdateUserRequestModel model);
+        string AddUser(AddUserRequestModel model);
+    }
+}
---
+++ Mission/Mission.Services/IServices/IMissionSkillService.cs
@@ -0,0 +1,17 @@
+﻿using Mission.Entities.Models;
+using System.Collections.Generic;
+using Mission.Services.IServices;
+
+namespace Mission.Services.IServices
+{
+   
+    public interface IMissionSkillService
+    {
+        IEnumerable<MissionSkillModel> GetMissionSkillList();
+        MissionSkillModel GetMissionSkillById(int id);
+        MissionSkillModel AddMissionSkill(AddMissionSkillRequestModel model);
+        MissionSkillModel UpdateMissionSkill(UpdateMissionSkillRequestModel model);
+        bool DeleteMissionSkill(int id);
+    }
+
+}
