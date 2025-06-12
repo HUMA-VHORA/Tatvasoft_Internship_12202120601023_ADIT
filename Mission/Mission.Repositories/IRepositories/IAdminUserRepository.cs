b0e8927f569893f2cd16343b833c924b7da04eaa
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-12 15:03:43 +0530

    Add files via upload

---
+++ Mission/Mission.Repositories/IRepositories/IAdminUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mission.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Repositories.Interface
+{
+    public interface IAdminUserRepository
+    {
+        List<UserDetails> UserDetailsList();
+        string UpdateUser(UpdateUserRequestModel model);
+        string AddUser(AddUserRequestModel model);
+    }
+}
