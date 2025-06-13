ce99625d22b5f4b82b67d4f176aa7a9322586dd7
HUMA-VHORA <vhorahuma37@gmail.com>
2025-06-13 20:38:04 +0530

    Add files via upload

--- Mission/Mission.Repositories/Repositories/AdminUserRepository.cs
+++ Mission/Mission.Repositories/Repositories/AdminUserRepository.cs
@@ -1,7 +1,4 @@
-﻿using Mission.Entities;
-using Mission.Entities.Context;
-using Mission.Entities.Models;
-using Mission.Repositories.Interface;
+﻿using Mission.Repositories.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,54 +7,7 @@ using System.Threading.Tasks;
 
 namespace Mission.Repositories
 {
-    public class AdminUserRepository(MissionDbContext _missionDb) : IAdminUserRepository
+    public class AdminUserRepository : IAdminUserRepository
     {
-        public List<UserDetails> UserDetailsList()
-        {
-            var res = _missionDb.User.Where(x => !x.IsDeleted && x.UserType == "user").Select(x => new UserDetails(x));
-            return res.ToList();
-        }
-        public string UpdateUser(UpdateUserRequestModel model)
-        {
-            var user = _missionDb.User.FirstOrDefault(x => x.Id == model.Id);
-            if (user == null)
-                throw new Exception("User not found");
-
-            user.FirstName = model.FirstName;
-            user.LastName = model.LastName;
-            user.PhoneNumber = model.PhoneNumber;
-            user.EmailAddress = model.EmailAddress;
-            user.ModifiedDate = DateTime.Now;
-
-            _missionDb.User.Update(user);
-            _missionDb.SaveChanges();
-
-            return "User updated!";
-        }
-        public string AddUser(AddUserRequestModel model)
-        {
-            // Optional: Check if user with same email exists to avoid duplicates
-            var existingUser = _missionDb.User.FirstOrDefault(x => x.EmailAddress == model.EmailAddress && !x.IsDeleted);
-            if (existingUser != null)
-                throw new Exception("User with this email already exists.");
-
-            var newUser = new User
-            {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                PhoneNumber = model.PhoneNumber,
-                EmailAddress = model.EmailAddress,
-                Password = model.Password, // Consider hashing password in production
-                UserType = model.UserType ?? "user",
-                IsDeleted = false,
-                CreatedDate = DateTime.UtcNow,
-                ModifiedDate = DateTime.UtcNow
-            };
-
-            _missionDb.User.Add(newUser);
-            _missionDb.SaveChanges();
-
-            return "User added successfully!";
-        }
     }
 }
--- Mission/Mission.Repositories/Repositories/LoginRepository.cs
+++ Mission/Mission.Repositories/Repositories/LoginRepository.cs
@@ -1,12 +1,17 @@
-﻿using Mission.Entities;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mission.Entities;
 using Mission.Entities.Context;
 using Mission.Entities.Models;
+using Mission.Entity.Entities;
 using Mission.Repositories.IRepositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Mission.Repositories
 {
@@ -41,27 +46,119 @@ namespace Mission.Repositories
             };
         }
 
-        public async Task<string> Register(RegisterUserModel model)
-        {
-            var isExist = _cIDbContext.User.Where(x => x.EmailAddress == model.EmailAddress && !x.IsDeleted).FirstOrDefault();
 
-            if (isExist != null) throw new Exception("Email already exist");
+        public async Task<string> RegisterUser(RegisterUserRequestModel registerUserRequest)
+        {
+            var isEmailExist = await _cIDbContext.User.FirstOrDefaultAsync(u => u.EmailAddress.ToLower() == registerUserRequest.EmailAddress.ToLower());
+
+            if (isEmailExist != null)  throw new Exception("User already exist");
 
             User user = new User()
             {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                EmailAddress = model.EmailAddress,
-                Password = model.Password,
-                PhoneNumber = model.PhoneNumber,
-                UserType = "user",
-                IsDeleted = false,
-                CreatedDate = DateTime.UtcNow,
+              FirstName = registerUserRequest.FirstName,
+              LastName = registerUserRequest.LastName,
+              EmailAddress=registerUserRequest.EmailAddress,
+              Password = registerUserRequest.Password,
+              PhoneNumber   =registerUserRequest.PhoneNumber,
+              UserType  =registerUserRequest.UserType,
+              CreatedDate = DateTime.UtcNow,
             };
 
             await _cIDbContext.User.AddAsync(user);
-            _cIDbContext.SaveChanges();
-            return "User Added!";
+            await _cIDbContext.SaveChangesAsync();
+            return "User registered!";
+        }
+        public UserResponseModel LoginUserDetailById(int id)
+        {
+            var userDetail = _cIDbContext.User
+                .Where(u => u.Id == id && !u.IsDeleted)
+                .Select(user => new UserResponseModel()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    PhoneNumber = user.PhoneNumber,
+                    EmailAddress = user.EmailAddress,
+                    UserType = user.UserType,
+                })
+                .FirstOrDefault() ?? throw new Exception("User not found.");
+
+            return userDetail;
+        }
+
+        public async Task<bool> LoginUserProfileUpdate(AddUserDetailsRequestModel requestModel)
+        {
+            try
+            {
+                var user = _cIDbContext.User.Where(x => x.Id == requestModel.UserId).FirstOrDefault();
+
+                if (user == null) throw new Exception("Not Found!");
+
+                var userDetails = _cIDbContext.UserDetails.Where(x => x.UserId == requestModel.UserId).FirstOrDefault();
+
+                if (userDetails == null)
+                {
+                    // Add User Details
+                    UserDetail userDetail = new UserDetail()
+                    {
+                        UserId = requestModel.UserId,
+                        Availability = requestModel.Avilability,
+                        CityId = requestModel.CityId,
+                        CountryId = requestModel.CountryId,
+                        Department = requestModel.Department,
+                        EmployeeId = requestModel.EmployeeId,
+                        LinkedInUrl = requestModel.LinkdInUrl,
+                        Manager = requestModel.Manager,
+                        MyProfile = requestModel.MyProfile,
+                        MySkills = requestModel.MySkills,
+                        Surname = requestModel.Surname,
+                        Name = requestModel.Name,
+                        UserImage = requestModel.UserImage,
+                        WhyIVolunteer = requestModel.WhyIVolunteer,
+                        Status = requestModel.Status,
+                        Title = requestModel.Title,
+                        
+                        IsDeleted = false,
+                        CreatedDate = DateTime.Now,
+                    };
+
+                    await _cIDbContext.UserDetails.AddAsync(userDetail);
+                } else
+                {
+                    // Update User Details
+                    userDetails.UserId = requestModel.UserId;
+                    userDetails.Availability = requestModel.Avilability;
+                    userDetails.CityId = requestModel.CityId;
+                    userDetails.CountryId = requestModel.CountryId;
+                    userDetails.Department = requestModel.Department;
+                    userDetails.EmployeeId = requestModel.EmployeeId;
+                    userDetails.LinkedInUrl = requestModel.LinkdInUrl;
+                    userDetails.Manager = requestModel.Manager;
+                    userDetails.MyProfile = requestModel.MyProfile;
+                    userDetails.MySkills = requestModel.MySkills;
+                    userDetails.Surname = requestModel.Surname;
+                    userDetails.Name = requestModel.Name;
+                    userDetails.UserImage = requestModel.UserImage;
+                    userDetails.WhyIVolunteer = requestModel.WhyIVolunteer;
+                    userDetails.Status = requestModel.Status;
+                    userDetails.Title = requestModel.Title;
+
+                    userDetails.ModifiedDate = DateTime.Now;
+
+                    _cIDbContext.UserDetails.Update(userDetails);
+                }
+
+                user.FirstName = requestModel.Name;
+                user.LastName = requestModel.Surname;
+
+                _cIDbContext.User.Update(user);
+                await _cIDbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
